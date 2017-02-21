22f009bf0ea6e6dba9fc1b17f14a78a913b08b00
lfajardo20 <lfajardo@asu.edu>
2017-02-20 23:21:12 -0700

    Update AccountServerFactory.java

--- src/banking/primitive/core/AccountServerFactory.java
+++ src/banking/primitive/core/AccountServerFactory.java
@@ -43,36 +43,3 @@ public class AccountServerFactory {
 		return new ServerSolution();
 	}
 }
-
-/*
- * File: AccountServerFactory.java
- * Author: kevingary
- * Date: Unknown
- * 
- * Description: contains factory for accountserver
- */
-
-package banking.primitive.core;
-
-
-public class AccountServerFactory {
-
-	protected static AccountServerFactory singleton = null;
-
-	protected AccountServerFactory() {
-
-	}
-
-	public static AccountServerFactory getMe() {
-		if (singleton == null) {
-			singleton = new AccountServerFactory();
-		}
-
-		return singleton;
-	}
-
-	public AccountServer lookup() {
-		return new ServerSolution();
-	}
-}
-
