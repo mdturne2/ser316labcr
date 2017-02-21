d37e8eed0171d4c151d8ada0197541a375576e20
Leirbag <lfajardo@asu.edu>
2017-02-20 22:07:33 -0700

    Placed the private method in the correct order.

--- src/banking/primitive/core/ServerSolution.java
+++ src/banking/primitive/core/ServerSolution.java
@@ -53,29 +53,6 @@ class ServerSolution implements AccountServer {
 			}
 		}
 	}
-	
-	private boolean _newAccountFactory(String type, String name, float balance)
-		throws IllegalArgumentException {
-		
-		if (accountMap.get(name) != null) return false;
-		
-		Account acc;
-		if ("Checking".equals(type)) {
-			acc = new Checking(name, balance);
-
-		} else if ("Savings".equals(type)) {
-			acc = new Savings(name, balance);
-
-		} else {
-			throw new IllegalArgumentException("Bad account type:" + type);
-		}
-		try {
-			accountMap.put(acc.getName(), acc);
-		} catch (Exception exc) {
-			return false;
-		}
-		return true;
-	}
 
 	public boolean newAccount(String type, String name, float balance) 
 		throws IllegalArgumentException {
@@ -135,5 +112,28 @@ class ServerSolution implements AccountServer {
 			}
 		}
 	}
+	
+	private boolean _newAccountFactory(String type, String name, float balance)
+			throws IllegalArgumentException {
+			
+			if (accountMap.get(name) != null) return false;
+			
+			Account acc;
+			if ("Checking".equals(type)) {
+				acc = new Checking(name, balance);
+
+			} else if ("Savings".equals(type)) {
+				acc = new Savings(name, balance);
+
+			} else {
+				throw new IllegalArgumentException("Bad account type:" + type);
+			}
+			try {
+				accountMap.put(acc.getName(), acc);
+			} catch (Exception exc) {
+				return false;
+			}
+			return true;
+		}
 
 }
