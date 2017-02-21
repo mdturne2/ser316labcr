0bea8c35dd730df6477cd8bf0c7179afeaf13118
dlahtine <dlahtine@asu.edu>
2017-02-20 18:48:13 -0700

    #13 Placed getter after constructors

--- src/banking/primitive/core/Savings.java
+++ src/banking/primitive/core/Savings.java
@@ -11,6 +11,8 @@ public class Savings extends Account {
 	public Savings(String name, float balance) throws IllegalArgumentException {
 		super(name, balance);
 	}
+	
+	public String getType() { return "Checking"; }
 
 	/**
 	 * A deposit comes with a fee of 50 cents per deposit
@@ -43,8 +45,6 @@ public class Savings extends Account {
 		}
 		return false;
 	}
-	
-	public String getType() { return "Checking"; }
 
 	public String toString() {
 		return "Savings: " + getName() + ": " + getBalance();
